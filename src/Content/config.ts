c16bb5234a482bf3de56517867b5dfb0473c9b15
ilrobermar <guerramartinezroberto@gmail.com>
2025-05-18 21:54:56 +0200

    Create config.ts

---
+++ src/Content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    date: z.string()
+  })
+});
+
+export const collections = {
+  posts,
+};
