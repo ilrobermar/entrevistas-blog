dae2a990864d408f8d43cc0c0258a1c4bf1833a5
ilrobermar <guerramartinezroberto@gmail.com>
2025-05-20 14:30:33 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server'
+  output: 'server',
+  adapter: netlify()
 });
