b5ca502eab1c2e1efc3a3acec1c057d37ea2bedc
ilrobermar <guerramartinezroberto@gmail.com>
2025-05-23 22:54:45 +0200

    Update config.ts

--- src/Content/config.ts
+++ src/Content/config.ts
@@ -1,12 +1,13 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const postsCollection = defineCollection({
+const posts = defineCollection({
   schema: z.object({
     title: z.string(),
-    slug: z.string()
+    slug: z.string(),
+    date: z.string(), // también podrías usar z.date() si quieres usar formato ISO
   }),
 });
 
 export const collections = {
-  posts: postsCollection,
+  posts,
 };
